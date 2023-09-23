86a7e393c189c5142c509a26e74e8d4191714f70
WallisonSantos <wallisontiagoassis206@outlook.com>
2023-09-23 19:18:59 -0300

    Commit version Post, Delete

--- src/main/java/com/virtual/program/controllers/PersonController.java
+++ src/main/java/com/virtual/program/controllers/PersonController.java
@@ -2,11 +2,11 @@ package com.virtual.program.controllers;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
-
 import com.virtual.program.entities.Person;
 import com.virtual.program.repositories.PersonRepository;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +17,24 @@ public class PersonController {
     
     PersonRepository repository;
 
-    @GetMapping("api/person/find")
+    @GetMapping("/api/person/find")
     public List<Person> getAllPerson() {    
         return repository.findAll();
     }
-    @PostMapping("api/person/save")
+ 
+    @GetMapping("/api/person/find/{id}")
+    public Person getByPersonId(@PathVariable Long id) {
+        return repository.findById(id).get();
+    }
+ 
+    @PostMapping("/api/person/save")
     public Person createPerson(@RequestBody Person person) {
         return repository.save(person);
+    }
+ 
+    @DeleteMapping("/api/person/delete/{id}")
+    public boolean deletePersonById(@PathVariable Long id) {
+        repository.deleteById(id);
+        return true;
     }
 }
